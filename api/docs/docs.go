aae8ebde6e734c951c53d8a7d43f70a5988f6531
Sharofiddin <sharofiddinmacbook701@gmail.com>
2024-08-15 15:18:24 +0500

    banner api test

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -3765,6 +3765,8 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
+	LeftDelim:        "{{",
+	RightDelim:       "}}",
 }
 
 func init() {
