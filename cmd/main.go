a72fbaf6021be4e895f00fd007f9eab9d768ac12
Sharofiddin <sharofiddinmacbook701@gmail.com>
2024-10-02 09:58:32 +0500

    time sleep 1 minut render

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"e-commerce/config"
 	"e-commerce/service"
 	"fmt"
+	"net/http"
+	"time"
 
 	postgres "e-commerce/storage/postgres"
 	"e-commerce/storage/redis"
@@ -12,6 +14,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/saidamir98/udevs_pkg/logger"
 )
+
+// Yangi qo'shilgan keepAlive funksiyasi
+func keepAlive(cfg *config.Config) {
+	for {
+		_, err := http.Get(fmt.Sprintf("http://localhost%s/ping", cfg.HTTPPort))
+		if err != nil {
+			fmt.Println("Ping yuborishda xatolik:", err)
+		} else {
+			fmt.Println("Ping muvaffaqiyatli yuborildi")
+		}
+		time.Sleep(1 * time.Minute)
+	}
+}
 
 func main() {
 	cfg := config.Load()
@@ -54,20 +69,17 @@ func main() {
 
 	api.NewApi(r, &cfg, pgconn, log, services)
 
+	// Yangi qo'shilgan: Keep-alive funksiyasini ishga tushirish
+	go keepAlive(&cfg)
+
+	// Yangi qo'shilgan: Ping endpointini qo'shish
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
 	fmt.Println("Listening server", cfg.PostgresHost+cfg.HTTPPort)
 	err = r.Run(cfg.HTTPPort)
-	// err = r.Run(cfg.ServerHost + cfg.HTTPPort)
 	if err != nil {
 		panic(err)
 	}
-
-	// rdb = redis.NewClient(&redis.Options{
-	// 	Addr: RedisAddr,
-	// })
-
-	// // Check Redis connection
-	// _, err := rdb.Ping(ctx).Result()
-	// if err != nil {
-	// 	log.Fatalf("Could not connect to Redis: %v", err)
-	// }
 }
